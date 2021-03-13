e394125860934d5b80095e4848aa8645791b0a6f
Rishi Pratap <rp0258@srmist.edu.in>
2021-03-14 00:07:37 +0530

    js

--- src/Avatar.js
+++ src/Avatar.js
@@ -9,16 +9,10 @@ const Avatar = (props) => {
     <div>
     <h1> Welcome to RishiPratap Website</h1>
     <p>Click to Reveal</p>
-    <div id="row">
-    <div id="column">
-    <div id="card">
     <Avatarlist id="1" name="rishi"/>
     <Avatarlist id="2" name="raaj"/>
     <Avatarlist id="3" name="rahull"/>
     <Avatarlist id="4" name="raam"/>
-    </div>
-    </div>
-    </div>
     <br></br>
     <div id="group">
     <button>Subscribe</button>
